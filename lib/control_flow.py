fdc27acde8e88b468794f892396f2c1492732643
Maxi-arch <mxmu025@gmail.com>
2024-11-26 14:23:30 +0300

    1stcommit

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,42 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
-
+    if username =='admin' and password =='12345':
+        print("Access granted") 
+    else:
+        print('Access denied')
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        print("Brisk")
+    elif 40 <= temperature < 65:  
+        print("It's a little chilly out there!")
+    elif temperature > 85:
+        print("It's too dang hot out there!")
+    else:
+        print("It's perfect out there!")
+
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num %3==0 and num %5==0 :
+        print("Fizzbuzz")
+    elif num %3==0 :
+        print("Fizz")
+    elif num %5==0:
+        print("Buzz")
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == '+':
+      print(num1+num2)
+    elif operation == '-':
+      print(num1-num2)
+    elif operation == '/':
+        print(num1/num2)  
+    elif operation == '*' :
+        print(num1*num2)
+
+
+admin_login(username="admin",password=12345)     
+hows_the_weather(temperature=25)  
+fizzbuzz(15)
+calculator(operation="*",num1=15,num2=3) 
+    
